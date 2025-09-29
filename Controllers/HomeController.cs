97af6bbcde7f94b4ce1201b58a92f59049461e40
EvelynEnc <evelyn2001mtd@gmail.com>
2025-09-29 09:39:57 -0300

    Tarjetas de adopción hechas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using FrontSantaRamona.AdopcionModels;
 using Microsoft.AspNetCore.Mvc;
 using PruebaFront.Models;
 
@@ -44,8 +45,15 @@ namespace PruebaFront.Controllers
 
         public IActionResult Adopcion()
         {
-            return View();
+            var mascotas = new List<Adopcion>
+    {
+        new Adopcion { Id = 1, Nombre = "Luna", Edad = "2 años", Estado = "Vacunada", Raza = "Labrador", ImagenUrl = "/images/adoptados/Labrador.luna.jpg" },
+        new Adopcion { Id = 2, Nombre = "Max", Edad = "3 años", Estado = "Castrado", Raza = "Beagle", ImagenUrl = "/images/adoptados/Beagle.max.jpg" }
+    };
+
+            return View(mascotas); // Busca Views/Home/Adopcion.cshtml
         }
+
 
 
 
