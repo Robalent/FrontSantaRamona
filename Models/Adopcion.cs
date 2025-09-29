fca85ee768c1f8ceb7877a524b656f8738f1c354
EvelynEnc <evelyn2001mtd@gmail.com>
2025-09-29 00:12:57 -0300

    Agregué modelo, controlador y vista de adopción

---
+++ Models/Adopcion.cs
@@ -0,0 +1,16 @@
+﻿namespace FrontSantaRamona.AdopcionModels
+{
+    public class Adopcion
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public required string Edad { get; set; }
+        public required string Estado { get; set; }
+        public required string Raza { get; set; }
+        public required string ImagenUrl { get; set; }
+        public string? Descripcion { get; set; }
+    }
+}
+
+
+
